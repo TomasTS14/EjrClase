4d8e58ae0443d80f69d1c8f1d47f0bdde38d6ae7
Tomas <210099@fppiramide.com>
2022-01-21 22:56:54 +0100

    Todo funciona

--- CDsOptativo/src/ColeccionCDs.java
+++ CDsOptativo/src/ColeccionCDs.java
@@ -1,4 +1,59 @@
 
+import java.util.ArrayList;
 public class ColeccionCDs {
+	
+	ArrayList<CD> coleccion;
+	
+	public ColeccionCDs() {
+		coleccion= new ArrayList<>();
+	}
+	public CD getCD(int index) {
+		
+		return coleccion.get(index);
+	}
+	public boolean addCD(CD cd) {
+		return coleccion.add(cd);
+	}
+	public void removeCD(int index) {
+		
+		int aux= index -1;
+		
+		 coleccion.remove(index);
+	}
+	
+	public int enQueDiscoCancion(String parteTiulo) {
+	
+		
+		
+			for(int i=0; i<coleccion.size(); i++) {
+				for(int j=0; j<coleccion.get(i).totalSize() ; j++) {
+					if (coleccion.get(i).dameCancion(j).dameTitulo().contains(parteTiulo)) {
+					
+						return i;
+						
+					}
+				}
+			}
+			
+		
+		return -1;
+	}
+	
+	@Override
+	
+	public String toString() {
+		String cadaCD="";
+		
+		for(int i=0; i<coleccion.size(); i++) {
+			cadaCD += (i+1)+")=============================\n";
+			cadaCD += coleccion.get(i);
+			cadaCD += "\n";
+		}
+		
+		return cadaCD;
+		
+	}
+		
+		
 
 }
