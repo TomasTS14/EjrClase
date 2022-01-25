b60ddb34111a0857959be77785bca11de512db17
Tomas <210099@fppiramide.com>
2022-01-25 10:24:11 +0100

    Agregue el ejr6 producto

---
+++ Ejr6_T5_TomasT/src/Producto.java
@@ -0,0 +1,29 @@
+/*
+ * Escribe una clase Producto con atributos codigo (int),
+ *  nombre (String), tipo (String), precio (double), 
+ *  stock (int). Implementa la clase ArrayProducto,
+ *   con un atributo de nombre lista y tipo ArrayList<Producto> 
+ *   y con los métodos agregar, eliminar, obtener (el producto a partir de su posición),
+ *    buscar (y recuperar el producto por su código), tamaño, imprimirLista, cambiarPrecio
+ *     (del producto mediante su código y el nuevo precio). Escribe después un programa para probar
+ *      la clase anterior.
+ */
+public class Producto {
+
+	private int codigo;
+	private String nombre;
+	private String tipo;
+	private double precio;
+	private int stock;
+	
+	public Producto() {
+		
+	}
+	
+	public 
+	
+	
+	
+	
+	
+}
