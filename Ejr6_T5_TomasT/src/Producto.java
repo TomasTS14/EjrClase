fbc9aa253ed31ad177bbc98f69015bfe81a76f56
Tomas <210099@fppiramide.com>
2022-01-26 13:35:48 +0100

    Agregue ejercicio 6

--- Ejr6_T5_TomasT/src/Producto.java
+++ Ejr6_T5_TomasT/src/Producto.java
@@ -20,7 +20,65 @@ public class Producto {
 		
 	}
 	
-	public 
+	public Producto(int codigo, String nombre, String tipo, double precio, int stock) {
+		this.codigo= codigo;
+		this.nombre= nombre;
+		this.tipo= tipo;
+		this.precio= precio;
+		this.stock= stock;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+	@Override
+	
+	public String toString() {
+		String productoEscrito="Codigo: "+getCodigo()+"\nNombre: "+getNombre()+
+				"\nTipo: "+getTipo()+"\nPrecio: "+getPrecio()+"\nStock: "+getStock()+"\n";
+		
+		return productoEscrito;
+		
+	}
+	
+	
 	
 	
 	
