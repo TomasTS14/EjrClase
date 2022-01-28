fb062f785c9bd6f8fd5a32f521d863a88391ebe9
Tomas <210099@fppiramide.com>
2022-01-28 12:53:14 +0100

    agregue comparacion

--- comparacionLinkedyArrayList/src/comparacionArrayListLinkedList.java
+++ comparacionLinkedyArrayList/src/comparacionArrayListLinkedList.java
@@ -16,6 +16,33 @@ public class comparacionArrayListLinkedList {
 
 	public static void main(String[] args) {
 		
+		LinkedList<Integer> laLinked = new LinkedList<>();
+		ArrayList<Integer> laArray= new ArrayList<>();
+		
+		//Comparacion de agregado no aleatorio para linked
+		
+		long tiempoAhora= System.currentTimeMillis();
+		
+		for(int i=0; i<20000000; i++) {
+			int randomNum=(int)(Math.random()*2500);
+			laLinked.add(randomNum);
+		}
+		long tiempoDespues= System.currentTimeMillis();
+		
+		long diferencia= tiempoDespues - tiempoAhora;
+		
+		System.out.println("Para agregar 20.000.000 LinkedList elementos tarda (milisegundos): "+ diferencia);
+		
+		
+		tiempoAhora= System.currentTimeMillis();
+		System.out.println(laLinked);
+		tiempoDespues= System.currentTimeMillis();
+		diferencia= tiempoDespues- tiempoAhora;
+		System.out.println("Para imprimir 20.000.000 LinkedList de elementos tardo(milisegundos): "+diferencia);
+		
+		
+		
+		
 
 	}
 
