a8e21351a0c8b64c4b800b48071c5d0367b51686
Tomas Casa WIn <210099@fppiramide.com>
2022-04-04 22:51:15 +0200

    11 t9 empezado

--- T9_Ejr10_TomasT/src/uso.java
+++ T9_Ejr10_TomasT/src/uso.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /*
  * Escribir un método que reciba por parámetro dos rutas correspondientes a un archivo de origen y otro de destino
@@ -13,11 +17,57 @@ public class uso {
 
 	public static void main(String[] args) {
 		
-		String directorioOrigen = args[0];
-		String directorioDestino = args[1];
 		
+		
+			String directorioOrigen = args[0];
+			String directorioDestino = args[1];
+			
+			boolean remplazar=false;
+			if  ( args[2].equals("true") ) {
+				remplazar = true;
+				
+			}else if ( args[2].equals("false")) {
+				 remplazar = false;		
+			}		
+			
 		File fileOrigen = new File(directorioOrigen);
-		
+		File fileDestino = new File(directorioDestino);
+		System.out.println(new File(".").getAbsolutePath()	);
+		try (FileInputStream fileIn = new FileInputStream(fileOrigen.getAbsolutePath());
+				FileOutputStream fileOut = new FileOutputStream(fileDestino.getAbsolutePath());){
+			
+			System.out.println("intentando copiar de '"+directorioOrigen +"' a '" + directorioDestino+"' Con opcion "+remplazar);
+			try {
+				TimeUnit.SECONDS.sleep(2);
+			}catch(Exception e) {
+				System.out.println("Fallo de sistema..");
+			}
+				if (fileDestino.isDirectory()) {
+					
+					fileIn.transferTo(fileOut);
+					
+				}
+				
+				if ( fileDestino.isFile() &&  fileDestino.exists() ) {
+					
+					 if (  remplazar == true) {
+						fileDestino.delete();
+						fileIn.transferTo(fileOut);
+						}else {
+							throw new Exception("Fallo copiando en archivo ya existente, parametro usado: false");
+						}
+					 
+					}else if (fileDestino.isFile() && !fileDestino.exists()) {
+						fileIn.transferTo(fileOut);
+				}
+				
+				System.out.println("Copiado con exito");
+				
+		}catch(IOException e){
+			e.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 
 	}
